9aedf19831ccb2fc35f48776fc6c93f7ac541987
George <isecrevezte@gmail.com>
2025-03-11 17:37:47 +0300

    Готовимся к миссии

--- flask/flask2/server.py
+++ flask/flask2/server.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='Домашняя страница',
-                           username=user)
+@app.route('/<word>')
+@app.route('/index/<word>')
+def index(word):
+    return render_template('base.html', title=word)
 
 
 if __name__ == '__main__':
